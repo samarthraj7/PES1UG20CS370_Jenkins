df5e052e016631539455a21609f51ebb8d1132ff
Samarth Rajendra <93866900+samarthraj7@users.noreply.github.com>
2023-02-19 22:34:01 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "I am samarth rajendra and my srn is pes1ug20cs370";
   return 0;
 }
